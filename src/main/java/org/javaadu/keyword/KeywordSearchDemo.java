907684ce7cdabc4bdef8e49ee04b0a5f9904a54c
duqicauc <nkduqi@126.com>
2023-02-11 23:32:27 +0800

    AC自动机学习：试用ahocorasick实现关键词匹配

---
+++ src/main/java/org/javaadu/keyword/KeywordSearchDemo.java
@@ -0,0 +1,21 @@
+package org.javaadu.keyword;
+
+import org.ahocorasick.trie.Emit;
+import org.ahocorasick.trie.Trie;
+
+import java.util.Collection;
+
+public class KeywordSearchDemo {
+
+    public static void main(String[] args) {
+        Trie trie = Trie.builder()
+                .addKeyword("hers")
+                .addKeyword("his")
+                .addKeyword("she")
+                .addKeyword("he")
+                .build();
+        Collection<Emit> emits = trie.parseText("ushers");
+
+        System.out.println(emits);
+    }
+}
